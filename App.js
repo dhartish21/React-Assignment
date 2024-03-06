0fc75f756381277bba9707c527918a465e1e905f
dhartish21 <134483999+dhartish21@users.noreply.github.com>
2024-03-06 13:21:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Table from './modules/Table';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Product List</h1>
+      <Table />
+    </div>
+  );
+};
+
+export default App;
